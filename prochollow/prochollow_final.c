70c60577c3cb90be4364a9a431d0247c05d85aff
mcdulltii <mcdulltii@gmail.com>
2020-05-23 22:04:03 +0800

    Fixed read line

--- prochollow/prochollow_final.c
+++ prochollow/prochollow_final.c
@@ -18,36 +18,33 @@ void parent(char* pid) {
     int status;
     int count = 1;
     char *addr[10], *start_addr, *end_addr;
-    pid_t pid_wait = strtol(pid, NULL, 10);
-    waitpid(pid_wait, &status, 0);
     char cwd[50];
+    pid_t pid_wait = strtol(pid, NULL, 10);
+    // Wait for child process to run PTRACE_TRACEME
+    waitpid(pid_wait, &status, 0);
+    // Retrieve pwd
     if (getcwd(cwd, sizeof(cwd)) == NULL) exit(1);
 
-    if (WIFSTOPPED(status) && WSTOPSIG(status) == SIGTRAP) {
-        while(strstr(maps(pid, count), cwd)) {
+    // Sighalt child process to rewrite its virtual memory
+    if (WIFSTOPPED(status) && WSTOPSIG(status) == SIGTRAP) {    
+        // Only retrieve memory map of CODE section
+        while(strstr(maps(pid, count), cwd) != NULL) {
+            // Retrieve start address
             addr[count] = maps(pid, count);
             if (count == 1) start_addr = strtok(addr[count], "-");
             count++;
         }
+        // Retrieve end address
         end_addr = strtok(maps(pid, count-1), " ");
         end_addr = strtok(end_addr, "-");
         end_addr = strtok(NULL, "-");
 
+        // Rewrite child process's virtual memory
         mem(pid, start_addr, end_addr);
 
         ptrace(PTRACE_CONT, pid, (char*)1, 0); // Continue suspended process
         //kill(pid_wait, SIGKILL); // Kill suspended process
     }
-}
-
-unsigned char* readFile(const char *name) {
-    FILE *fl = fopen(name, "r");
-    fseek(fl, 0, SEEK_END);
-    long len = ftell(fl);
-    unsigned char *ret = malloc(len);
-    fseek(fl, 0, SEEK_SET);
-    fread(ret, 1, len, fl);
-    return ret;
 }
 
 int main(int argc, char* argv[]) {
@@ -56,6 +53,7 @@ int main(int argc, char* argv[]) {
     int pid;
 	char *pid_buf=(char*)malloc(sizeof(char*));
 
+    // Create pipe for transfer of pid
 	if (pipe(pid_pipe)==-1) {
         puts("ERROR");
         return 1;
@@ -70,6 +68,7 @@ int main(int argc, char* argv[]) {
 		write(pid_pipe[1], pid_buf, strlen(pid_buf)+1);
 		close(pid_pipe[1]);
 		ptrace(PTRACE_TRACEME, 0, 0, 0);
+        // Runs hello process with our argv, simulating the hello process
 		execv("./hello", argv);
 		free(pid_buf);
         break;
@@ -88,13 +87,14 @@ int main(int argc, char* argv[]) {
 char* maps(char* pid, int linesearch) {
     FILE *ptr;
     int line = 1, found = 0;
-    char* fl, liner[100], *rtn = (char*)malloc(sizeof(char*));
+    char* fl, liner[500], *rtn = (char*)malloc(sizeof(char*));
     fl = (char*)malloc(sizeof(char*));
     sprintf(fl, "/proc/%s/maps", pid);
     ptr = fopen(fl,"r");
     if (ptr == NULL) exit(1);
     
-    while(fgets(liner, 100, ptr)) {
+    // Retrieves line with linesearch from /proc/{pid}/maps
+    while(fgets(liner, 500, ptr)) {
         if (linesearch == line) {
             found = 1;
             break;
@@ -122,6 +122,7 @@ int mem(char* pid, char* start_addr, char* end_addr) {
     
     lseek(fd, s_addr, SEEK_SET);
     read(fd, buf, length);
+    // Check bytes for byte sequence of main function in child process to obtain offset address
     for (i=0; i<length; i++) {
         if ((int)buf[i] != 0x55) continue;
         if ((int)buf[i+1] != 0x48) continue;
@@ -131,6 +132,7 @@ int mem(char* pid, char* start_addr, char* end_addr) {
         break;
     }
     
+    // Rewrite specific/all bytes i.e. 0x0a -> 0x10
     lseek(fd, s_addr+offset+47, SEEK_SET);
     buf2 = (char[]) {0x10};
     if (write(fd, buf2, 1) == -1) return 1;
