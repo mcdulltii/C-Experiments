63fc53c4ca3251dff4238e9593d9b63fc6d56191
root <root@localhost.localdomain>
2020-06-01 04:38:48 -0400

    Improved corrupt solve

--- corrupt/solve.py
+++ corrupt/solve.py
@@ -6,6 +6,20 @@ index = f.find('\x00'*10)
 
 encoded_flag = f[index:].replace('\x00'*23,'')
 
-flag = ''.join([chr((ord(i) ^ ord('9')) - ord('d')) for i in encoded_flag])
+def decode(ef):
+    possible_flags = []
+    for xor in range(0, 127):
+        for sub in range(0, 127):
+            try:
+                flag = ''.join([chr((ord(i) ^ xor) - sub) for i in ef])
+                if "ASM" in flag or "A5M" in flag or "asm" in flag or "a5m" in flag:
+                    possible_flags.append(flag)
+            except:
+                continue
+    return [flag for flag in possible_flags if flag[-1] == '\n']
 
-print flag
+decoded_flag = decode(encoded_flag)
+
+assert len(decoded_flag) == 1
+
+print decoded_flag[0]
